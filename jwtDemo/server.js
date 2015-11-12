90b383360c60ed10f9a168a3fd1959fc20f55499
unknown <kevinbatchelor@gmail.com>
2015-11-11 23:07:05 -0700

    luck

--- jwtDemo/server.js
+++ jwtDemo/server.js
@@ -2,33 +2,34 @@ var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 var app = express();
+var jwtSecret = 'lakfja;ldskjf';
 
 var user = {
-    username:'user',
-    password:'pw'
+    username: 'user',
+    password: 'pw'
 };
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/random-user', function(req, res) {
+app.get('/random-user', function (req, res) {
     var user = faker.helpers.userCard();
     user.avatar = faker.image.avatar();
     res.json(user);
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
     console.log('App listening on localhost 3000')
 });
 
-var authenticate = function(req, res, next) {
+var authenticate = function (req, res, next) {
     var body = req.body;
-    console.log(req.body);
     if (!body.username || !body.password) {
         res.status(400).end('Must provide username and password')
     }
-    if(body.username !==user.username || body.password !== user.password){
+    if (body.username !== user.username || body.password !== user.password) {
         res.status(401).end(
             'password is bogus'
         )
@@ -36,7 +37,11 @@ var authenticate = function(req, res, next) {
     next();
 };
 
-app.post('/login', authenticate, function(req, res) {
- res.send(user)
+app.post('/login', authenticate, function (req, res) {
+    var token = jwt.sign({username: user.username}, jwtSecret);
+    res.send({
+        token: token,
+        user: user
+    })
 });
 
